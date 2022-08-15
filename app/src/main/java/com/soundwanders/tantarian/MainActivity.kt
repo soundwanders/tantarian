00cf609da8b0e089ff5027a2a693d65eaa1207df
soundwanders <69735204+soundwanders@users.noreply.github.com>
2022-08-14 22:24:09 -0400

    basic UI design

--- app/src/main/java/com/soundwanders/tantarian/MainActivity.kt
+++ app/src/main/java/com/soundwanders/tantarian/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.soundwanders.tantarian
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
@@ -18,11 +19,11 @@ class MainActivity : AppCompatActivity() {
 
         // handle button clicks
         binding.loginBtn.setOnClickListener {
-
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
         binding.skipBtn.setOnClickListener {
-
+            startActivity(Intent(this, UserDashboardActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/soundwanders/tantarian/SplashActivity.kt
+++ app/src/main/java/com/soundwanders/tantarian/SplashActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed(Runnable {
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
             startActivity(Intent(this, MainActivity::class.java))
         }, 2000) // 2 second delay
     }
