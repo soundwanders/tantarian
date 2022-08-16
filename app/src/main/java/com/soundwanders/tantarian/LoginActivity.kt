5e5db0398ab354c5ae57f914becdc641e5918d98
soundwanders <69735204+soundwanders@users.noreply.github.com>
2022-08-15 22:01:09 -0400

    user login/register with firebase auth

--- app/src/main/java/com/soundwanders/tantarian/LoginActivity.kt
+++ app/src/main/java/com/soundwanders/tantarian/LoginActivity.kt
@@ -1,11 +1,115 @@
 package com.soundwanders.tantarian
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.soundwanders.tantarian.databinding.ActivityLoginBinding
+import com.soundwanders.tantarian.databinding.ActivityRegisterBinding
 
 class LoginActivity : AppCompatActivity() {
+    // view
+    private lateinit var binding: ActivityLoginBinding
+
+    // firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // PROGRESS DIALOG
+    // *** DEPRECATED *** DEPRECATED -- FIND REPLACEMENT
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Loading...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        // if no user account, send to sign up page
+        binding.signUpTv.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        binding.loginBtn.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private var email = ""
+    private var password = ""
+
+    private fun validateData() {
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Invalid e-mail, please try again", Toast.LENGTH_SHORT).show()
+        } else if (password.isEmpty()) {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+        } else {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        // show progress
+        progressDialog.setMessage("Logging in to your account...")
+        progressDialog.show()
+
+        // createUserWithEmailAndPassword is from Firebase API ...
+        // public Task<AuthResult> createUserWithEmailAndPassword (String email, String password)
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                checkUser()
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(
+                    this,
+                    "Failed to log in due to ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun checkUser() {
+        progressDialog.setMessage("Checking user...")
+
+        val firebaseUser = firebaseAuth.currentUser!!
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseUser.uid)
+            .addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    progressDialog.dismiss()
+
+                    // get user type (potential value is either User or Admin)
+                    val userType = snapshot.child("userType").value
+
+                    // navigate to appropriate dashboard depending on user type
+                    if (userType == "user") {
+                        startActivity(Intent(this@LoginActivity, UserDashboardActivity::class.java))
+                    }
+                    else if (userType == "admin") {
+                        startActivity(Intent(this@LoginActivity, AdminDashboardActivity::class.java))
+                        finish()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
     }
 }
--- app/src/main/java/com/soundwanders/tantarian/RegisterActivity.kt
+++ app/src/main/java/com/soundwanders/tantarian/RegisterActivity.kt
@@ -1,11 +1,130 @@
 package com.soundwanders.tantarian
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.soundwanders.tantarian.databinding.ActivityRegisterBinding
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
+    // view
+    private lateinit var binding: ActivityRegisterBinding
+
+    // firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // PROGRESS DIALOG
+    // *** DEPRECATED *** DEPRECATED -- FIND REPLACEMENT
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Loading...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.backBtn.setOnClickListener {
+            // navigate to previous screen
+            onBackPressed()
+        }
+
+        binding.registerBtn.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private var name = ""
+    private var email = ""
+    private var password = ""
+
+    private fun validateData() {
+        // input data
+
+        name = binding.nameEt.text.toString().trim()
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+        val confirmPassword = binding.confirmPasswordEt.text.toString().trim()
+
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_SHORT).show()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Invalid e-mail, please try again", Toast.LENGTH_SHORT).show()
+        } else if (password.isEmpty()) {
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+
+        } else if (confirmPassword.isEmpty()) {
+            Toast.makeText(this, "Please confirm your password", Toast.LENGTH_SHORT).show()
+        } else if (confirmPassword != password) {
+            Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show()
+        } else {
+            createUserAccount()
+        }
+    }
+
+    private fun createUserAccount() {
+        // show progress
+        progressDialog.setMessage("Creating your account...")
+        progressDialog.show()
+
+        // createUserWithEmailAndPassword is from Firebase API ...
+        // public Task<AuthResult> createUserWithEmailAndPassword (String email, String password)
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                updateUserInfo()
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(
+                    this,
+                    "Failed to create account due to ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun updateUserInfo() {
+        progressDialog.setMessage("Saving updated user info...")
+
+        val timestamp = System.currentTimeMillis()
+        val uid = firebaseAuth.uid
+
+        val hashMap: HashMap<String, Any?> = HashMap()
+        hashMap["uid"] = uid
+        hashMap["email"] = email
+        hashMap["name"] = name
+        hashMap["profileAvatar"] = ""
+        hashMap["userType"] = "user"
+        hashMap["timestamp"] = timestamp
+
+        // send data to db
+        // (uid!!) ensures child is a String
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+
+        ref.child(uid!!)
+        .setValue(hashMap)
+        .addOnSuccessListener {
+            progressDialog.dismiss()
+            Toast.makeText(this, "Account created!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@RegisterActivity, UserDashboardActivity::class.java))
+            finish()
+        }
+        .addOnFailureListener { e ->
+            progressDialog.dismiss()
+            Toast.makeText(
+                this,
+                "Failed to register new user due to ${e.message}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
